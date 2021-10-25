c587e49bb7982014aee720d2bfcdf5dabbabe73f
Matt Wojcieszek <wojo1330@gmail.com>
2021-10-25 18:27:47 -0400

    create two rows by defining twice

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import "./styles.css";
 
+// Lilly	Minchala	She/Hers	HHSLT	2022	Purple	Insta	Rats	Lillian
+//Luna-Marcelino	She/Hers	P-TECH	2022			Cats	Luna
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      fdsfds
+      <div class="row">
+        <div class="red">Lilly Minchala</div>
+        <div>She/Hers</div>
+        <div class="red">HHSLT</div>
+        <div>2022</div>
+        <div class="red">divurdivle</div>
+        <div>Insta</div>
+        <div class="red">Rats</div>
+        <div>Lillian</div>
+      </div>
+
+      <div class="row">
+        <div class="red">Lilly Minchala</div>
+        <div>She/Hers</div>
+        <div class="red">HHSLT</div>
+        <div>2022</div>
+        <div class="red">Luna-Marcelino</div>
+        <div>Insta</div>
+        <div class="red">Rats</div>
+        <div>Lillian</div>
+      </div>
     </div>
   );
 }
